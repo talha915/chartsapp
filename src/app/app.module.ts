02f9aa3a66f9196db6b75a61976a2ac2536d0cf8
talha915 <talhazafar915426@gmail.com>
2018-06-26 18:06:25 +0500

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ChartModule } from 'angular2-highcharts'; 
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppComponent } from './app.component';
+
+declare var require: any;
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartModule.forRoot(require('highcharts'))
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+platformBrowserDynamic().bootstrapModule(AppModule);
---
+++ src/app/simplechart/simplechart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simplechart',
+  templateUrl: './simplechart.component.html',
+  styleUrls: ['./simplechart.component.css']
+})
+export class SimplechartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
